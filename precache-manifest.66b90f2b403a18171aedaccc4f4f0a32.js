cf27abe9bb2ab19b3c44e67cf8056d76c5d6847b
Valyoung <valeshkayoungsang@yahoo.com>
2020-05-07 21:17:54 -0500

    Updates

---
+++ precache-manifest.66b90f2b403a18171aedaccc4f4f0a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ce909f650bb7a24f4d13ccc68e2c23",
+    "url": "/My-React-Portfolio/index.html"
+  },
+  {
+    "revision": "f8ed84352180f553000e",
+    "url": "/My-React-Portfolio/static/css/2.dbce72c5.chunk.css"
+  },
+  {
+    "revision": "15465bf99cebab56b538",
+    "url": "/My-React-Portfolio/static/css/main.da01d871.chunk.css"
+  },
+  {
+    "revision": "f8ed84352180f553000e",
+    "url": "/My-React-Portfolio/static/js/2.424d02a8.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/My-React-Portfolio/static/js/2.424d02a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15465bf99cebab56b538",
+    "url": "/My-React-Portfolio/static/js/main.1c1f0059.chunk.js"
+  },
+  {
+    "revision": "098007f89f0c4b29dcbc",
+    "url": "/My-React-Portfolio/static/js/runtime-main.3a680cda.js"
+  }
+]);
